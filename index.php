d7dd73210f46059bf54c18d496bcb68d78ad6dd2
Zakaria Abbasi <zakaria_abbasi@yahoo.com>
2022-01-09 10:58:19 +0330

    test router

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 # front controller
+
+use App\Core\StupidRouter;
+
 ini_set('display_errors', E_ALL);
-use App\Utilities\Url;
+
 include_once __DIR__.'/bootstrap/init.php';
 
-$route = Url::current_route();
-
-
-if($route == '/colors/blue')
-    include_once BASE_PATH."views/colors/blue.php";
-if($route == '/colors/green')
-    include_once BASE_PATH."views/colors/green.php";
-if($route == '/colors/red')
-    include_once BASE_PATH."views/colors/red.php";
+$route = new StupidRouter();
+$route->ran();
